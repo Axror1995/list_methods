61e7e1e3f325ab1a5ef2179306fcd87b84f9cf71
Axror1995 <axrorraximov108@gmail.com>
2023-03-14 16:52:56 +0900

    list10.dart

--- bin/list_method10.dart
+++ bin/list_method10.dart
@@ -7,4 +7,25 @@
         list: return answer
 */
 
-void main() {}
+List func(List fruits) {
+  int a = fruits.length;
+  int b = 0;
+  int ans1 = 0;
+  int ans2 = 0;
+  List answer = [];
+  while (b < a) {
+    if (fruits[b] == 0) {
+      ans1++;
+    }
+    if (fruits[b] == 1) {
+      ans2++;
+    }
+    b += 1;
+  }
+  [answer=[ans2,ans1]];
+  return answer;
+}
+
+void main() {
+  print(func([1, 1, 1, 1, 1, 0, 0, 1]));
+}
